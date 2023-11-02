1a1fab9b2cf6923330a84f2fc95dc3a2c81b7e9d
Sammy Metref <metrefsammy@gmail.com>
2023-11-02 12:19:09 +0100

    compute pdf and save fig (demo_pdfvars_swot.ipynb)

--- docs/build/html/_sources/4_metrics_det/metrics_2-physicalpdf.md.txt
+++ docs/build/html/_sources/4_metrics_det/metrics_2-physicalpdf.md.txt
@@ -4,3 +4,38 @@
  
 
 <br> 
+
+ 
+
+## 1 day orbit - Gulf Stream example
+
+**SSH**
+
+![1d GS ssh](../gallery/pdf_Unet_baseline_GS_1d_ssh.png)
+
+**Gradients of SSH**
+
+![1d GS ssh](../gallery/pdf_Unet_baseline_GS_1d_grad.png)
+
+**Laplacian of SSH** 
+
+![1d GS ssh](../gallery/pdf_Unet_baseline_GS_1d_lapl.png)
+
+## 21 day orbit - Gulf Stream example
+ 
+**SSH**
+
+![1d GS ssh](../gallery/pdf_Unet_baseline_GS_21d_ssh.png)
+
+**Gradients of SSH**
+
+![1d GS ssh](../gallery/pdf_Unet_baseline_GS_21d_grad.png)
+
+**Laplacian of SSH** 
+
+![1d GS ssh](../gallery/pdf_Unet_baseline_GS_21d_lapl.png)
+
+<br> 
+ 
+<br>
+
--- src/dict_regions_info.py
+++ src/dict_regions_info.py
@@ -21,8 +21,8 @@ GS_info = {
     'lon_max' : -45,                            # domain max longitude
     'lat_min' : 30,                             # domain min latitude
     'lat_max' : 40,                              # domain max latitude
-    'lon_ticks' : ['55$^\circ$ W','53$^\circ$ W','51$^\circ$ W','49$^\circ$ W','47$^\circ$ W'], 
-    'lat_ticks' : ['30$^\circ$ N','32$^\circ$ N','34$^\circ$ N','36$^\circ$ N','38$^\circ$ N']
+    'lon_ticks' : ['55$^\circ$ W','53$^\circ$ W','51$^\circ$ W','49$^\circ$ W','47$^\circ$ W'], # ticks label for plots
+    'lat_ticks' : ['30$^\circ$ N','32$^\circ$ N','34$^\circ$ N','36$^\circ$ N','38$^\circ$ N']  # ticks label for plots
 }
 
 Med_info = {
@@ -30,16 +30,20 @@ Med_info = {
     'lon_min' : -2,                              # domain min longitude
     'lon_max' : 8,                             # domain max longitude
     'lat_min' : 36,                             # domain min latitude
-    'lat_max' : 44                              # domain max latitude
+    'lat_max' : 44,                              # domain max latitude
+    'lon_ticks' : ['2$^\circ$ W','0$^\circ$','2$^\circ$ E','4$^\circ$ E','6$^\circ$ E'],  # ticks label for plots
+    'lat_ticks' : ['36$^\circ$ N','38$^\circ$ N','40$^\circ$ N','42$^\circ$ N'] # ticks label for plots
 }
 
 
 ACC_info = {
     'name' : 'ACC',                              # region name
-    'lon_min' : 152.,                             # domain min longitude
-    'lon_max' : 162.,                             # domain max longitude
-    'lat_min' : -64.,                             # domain min latitude
-    'lat_max' : -54.                              # domain max latitude
+    'lon_min' : 152,                             # domain min longitude
+    'lon_max' : 162,                             # domain max longitude
+    'lat_min' : -64,                             # domain min latitude
+    'lat_max' : -54,                              # domain max latitude
+    'lon_ticks' : ['152$^\circ$ E','154$^\circ$ E','156$^\circ$ E','158$^\circ$ E','160$^\circ$ E'],  # ticks label for plots
+    'lat_ticks' : ['64$^\circ$ S','62$^\circ$ S','60$^\circ$ S','58$^\circ$ S','56$^\circ$ S'] # ticks label for plots
 }
 
 Mada_info = {
@@ -47,7 +51,9 @@ Mada_info = {
     'lon_min' : 44,                              # domain min longitude
     'lon_max' : 54,                             # domain max longitude
     'lat_min' : -12,                             # domain min latitude
-    'lat_max' : -2                              # domain max latitude
+    'lat_max' : -2,                              # domain max latitude
+    'lon_ticks' : ['44$^\circ$ E','46$^\circ$ E','48$^\circ$ E','50$^\circ$ E','52$^\circ$ E'],  # ticks label for plots
+    'lat_ticks' : ['12$^\circ$ S','10$^\circ$ S','8$^\circ$ S','6$^\circ$ S','4$^\circ$ S'] # ticks label for plots
 }
 
 
@@ -56,5 +62,7 @@ Cali_info = {
     'lon_min' : -130,                             # domain min longitude
     'lon_max' : -120,                             # domain max longitude
     'lat_min' : 30,                             # domain min latitude
-    'lat_max' : 40                              # domain max latitude
+    'lat_max' : 40,                              # domain max latitude
+    'lon_ticks' : ['130$^\circ$ W','128$^\circ$ W','126$^\circ$ W','124$^\circ$ W','122$^\circ$ W'],  # ticks label for plots
+    'lat_ticks' : ['30$^\circ$ N','32$^\circ$ N','34$^\circ$ N','36$^\circ$ N','38$^\circ$ N'] # ticks label for plots
 }
